f823d69521911975ac3549cbf81af2a223329fb8
Fabio Almeida <falmeida.tech@gmail.com>
2020-09-04 23:44:07 -0400

    Added a new function

--- go-pkg-sample/main.go
+++ go-pkg-sample/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"fmt"
 	"go_samples/go-pkg-sample/stringutils"
+	"go_samples/go-pkg-sample/calc"
 )
 
 func main(){
 	s:="fabio"
 	fmt.Println(stringutils.Upper(s))
-
+	result := calc.Add(2,3)
+	println(result)
 	printMemoryAddr()
 }
 
